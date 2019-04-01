9c68eceb0db1772a252621b6448cd69003b29fb6
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-03-31 23:59:46 -0400

    Remove methods moved to promtest package

--- prometheusx/prom.go
+++ prometheusx/prom.go
@@ -1,24 +1,16 @@
-// Package prometheusx provides a canonical way to expose Prometheus metrics
-// and provides a utility function for linting those metrics.
+// Package prometheusx provides a canonical way to expose Prometheus metrics.
 package prometheusx
 
 import (
-	"bytes"
-	"context"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/pprof"
 	"strconv"
-	"testing"
 
 	"github.com/m-lab/go/httpx"
 	"github.com/m-lab/go/rtx"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/prometheus/util/promlint"
 )
 
 var (
@@ -79,34 +71,4 @@ func MustStartPrometheus(addr string) *http.Server {
 
 	// Return the server
 	return server
-}
-
-// LintMetrics will ensure that the names of the passed-in Promethus metrics
-// follow all best practices. If the passed-in testing.T is nil, then all lint
-// errors are just log messages. If a real testing.T is passed in, then lint
-// errors cause test failures.
-func LintMetrics(t *testing.T) (passed bool) {
-	srv := MustStartPrometheus(":0")
-	defer srv.Shutdown(context.Background())
-
-	metricReader, err := http.Get("http://" + srv.Addr + "/metrics")
-	rtx.Must(err, "Could not GET metrics")
-	metricBytes, err := ioutil.ReadAll(metricReader.Body)
-	rtx.Must(err, "Could not read metrics")
-
-	metricsLinter := promlint.New(bytes.NewBuffer(metricBytes))
-	problems, err := metricsLinter.Lint()
-	rtx.Must(err, "Could not lint metrics")
-
-	passed = true
-	for _, p := range problems {
-		passed = false
-		msg := fmt.Sprintf("Bad metric %v: %v", p.Metric, p.Text)
-		if t == nil {
-			log.Println(msg)
-		} else {
-			t.Error(msg)
-		}
-	}
-	return passed
 }
