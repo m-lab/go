48fa3e32b6cc9654695e9a577d14d341ec042897
Greg Russell <gfr10598@gmail.com>
2019-04-18 14:17:36 -0400

    Add unit tests

--- logx/logx.go
+++ logx/logx.go
@@ -1,12 +1,50 @@
 // Package logx provides very useful utilities for logging.
 // I hesitate to create it, for fear we will add too much stuff to it.  But LogEvery seems worth it.
+// This packages uses the MIT license, to respect the corresponding license from kami-zh/go-capture
 package logx
 
+/*
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to
+do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+*/
+
 import (
+	"bytes"
+	"io"
+	"log"
+	"os"
 	"sync/atomic"
 	"time"
 	"unsafe"
 )
+
+// CaptureLog captures all output from log.Println, etc.
+// Adapted from github.com/kami-zh
+func CaptureLog(f func()) string {
+	r, w, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+
+	log.SetOutput(w)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+
+	f()
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	return buf.String()
+}
 
 // A Logger has basic Printf and Println functions.
 type Logger interface {
@@ -19,26 +57,31 @@ type logEvery struct {
 	interval time.Duration
 }
 
-// Should be handled with atomic update
-var lastDecodeLogTime = unsafe.Pointer(&time.Time{})
-
 // NewLogEvery creates a logger that will log not more than once every interval.
 func NewLogEvery(interval time.Duration) Logger {
 	return &logEvery{unsafe.Pointer(&time.Time{}), interval}
 }
 
-func (log *logEvery) Println(v ...interface{}) {
+func (le *logEvery) ok() bool {
+	now := time.Now()
+	oldPtr := atomic.LoadPointer(&le.lastTime)
+	last := *(*time.Time)(oldPtr)
+	if now.Sub(last) < le.interval {
+		return false
+	}
+	// If this fails, then some other thread won the race.
+	return atomic.CompareAndSwapPointer(&le.lastTime, oldPtr, unsafe.Pointer(&now))
+}
+
+func (le *logEvery) Println(v ...interface{}) {
+	if le.ok() {
+		log.Println(v...)
+	}
 }
 
 // LogEvery takes an interval and pointer to a time.Time, and determines whether to produce the log or not.
-func (log *logEvery) Printf(fmt string, v ...interface{}) {
-	now := time.Now()
-	oldPtr := atomic.LoadPointer(&log.lastTime)
-	last := *(*time.Time)(oldPtr)
-	if now.Sub(last) < log.interval {
-		return
-	}
-	if atomic.CompareAndSwapPointer(&log.lastTime, oldPtr, unsafe.Pointer(&now)) {
-		log.Println(v...)
+func (le *logEvery) Printf(fmt string, v ...interface{}) {
+	if le.ok() {
+		log.Printf(fmt, v...)
 	}
 }
---
+++ logx/logx_test.go
@@ -0,0 +1,78 @@
+package logx_test
+
+import (
+	"log"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/m-lab/go/logx"
+)
+
+func init() {
+	// Always prepend the filename and line number.
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func TestCaptureLog(t *testing.T) {
+	out := logx.CaptureLog(func() {
+		logger := logx.NewLogEvery(time.Millisecond)
+		start := time.Now()
+		log.Println("key phrase")
+		for time.Since(start) < 10*time.Millisecond {
+			logger.Println(time.Now())
+		}
+	})
+
+	if !strings.Contains(out, "logx_test.go:") {
+		t.Error("Missing short filename")
+	}
+
+	if !strings.Contains(out, "key phrase") {
+		t.Error("Missing key phrase")
+	}
+}
+
+func TestLogEvery_Println(t *testing.T) {
+	out := logx.CaptureLog(func() {
+		logger := logx.NewLogEvery(time.Millisecond)
+		start := time.Now()
+		for ; time.Since(start) < 10*time.Millisecond; time.Sleep(100 * time.Microsecond) {
+			logger.Println("foobar")
+		}
+	})
+
+	if !strings.Contains(out, "logx.go:") {
+		t.Error("Missing short filename")
+	}
+	lines := strings.Split(out, "\n")
+	// Should be 10 or 11.  Error if more than 12.
+	if len(lines) > 12 {
+		t.Error("Too many logs", len(lines))
+	}
+	if len(lines) < 9 {
+		t.Error("Too few logs", len(lines))
+	}
+}
+
+func TestLogEvery_Printf(t *testing.T) {
+	out := logx.CaptureLog(func() {
+		logger := logx.NewLogEvery(time.Millisecond)
+		start := time.Now()
+		for ; time.Since(start) < 10*time.Millisecond; time.Sleep(100 * time.Microsecond) {
+			logger.Printf("%s\n", "foobar")
+		}
+	})
+
+	if !strings.Contains(out, "logx.go:") {
+		t.Error("Missing short filename")
+	}
+	lines := strings.Split(out, "\n")
+	// Should be 10 or 11.  Error if more than 12.
+	if len(lines) > 12 {
+		t.Error("Too many logs", len(lines))
+	}
+	if len(lines) < 9 {
+		t.Error("Too few logs", len(lines))
+	}
+}
