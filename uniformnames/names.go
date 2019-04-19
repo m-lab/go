78bd75cc55cda6c8fd63459ff33117e393e17e21
Peter Boothe <pboothe@google.com>
2019-04-19 13:57:20 -0400

    Check uniform naming convention. (#54)
    Check names against uniform naming conventions.

---
+++ uniformnames/names.go
@@ -0,0 +1,21 @@
+// Package uniformnames allows experiments and utilities to check whether names
+// conform to the requirements for experiment and datatype names in M-Lab.
+package uniformnames
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	nameRE = regexp.MustCompile("^[a-z][a-z0-9]*$")
+)
+
+// Check whether a passed-in name conforms to the specs set out in the "M-Lab
+// Unform Naming" doc.
+func Check(name string) error {
+	if nameRE.MatchString(name) {
+		return nil
+	}
+	return fmt.Errorf("%q did not match the regexp %q", name, nameRE.String())
+}
---
+++ uniformnames/names_test.go
@@ -0,0 +1,35 @@
+package uniformnames_test
+
+import (
+	"testing"
+
+	"github.com/m-lab/go/uniformnames"
+)
+
+func TestNames(t *testing.T) {
+	for _, goodname := range []string{
+		"good",
+		"good123",
+		"good123abc",
+		"finealso",
+	} {
+		if uniformnames.Check(goodname) != nil {
+			t.Errorf("%q was a good name but did not pass the check", goodname)
+		}
+	}
+	for _, badname := range []string{
+		"1bad",
+		"bad-123",
+		"bad123ABC",
+		"AlsoBad",
+		"not_okay",
+		"!nope",
+		"",
+		":(",
+		"abd.def",
+	} {
+		if uniformnames.Check(badname) == nil {
+			t.Errorf("%q was a bad name but passed the check", badname)
+		}
+	}
+}
