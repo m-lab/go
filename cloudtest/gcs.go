66aa6ab322ac90d8eb41066946359d6e332c8c3a
Gregory Russell <gfr10598@users.noreply.github.com>
2019-07-17 10:15:37 -0400

    Add GCSClient for storage testing (#60)
    * Add GCSClient for storage testing
    
    * fix query handling, better testing
    
    * move assert to test
    
    * comments and readability
    
    * add license and update package comment
    
    * more comments

---
+++ cloudtest/gcs.go
@@ -0,0 +1,103 @@
+//  Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package cloudtest provides utilities for testing, e.g. cloud
+// service tests using mock http Transport, fake storage client, etc.
+package cloudtest
+
+import (
+	"context"
+	"strings"
+
+	"cloud.google.com/go/storage"
+	"github.com/GoogleCloudPlatform/google-cloud-go-testing/storage/stiface"
+	"google.golang.org/api/iterator"
+)
+
+// GCSClient provides a fake storage client that can be customized with arbitrary fake bucket contents.
+type GCSClient struct {
+	stiface.Client
+	buckets map[string]BucketHandle
+}
+
+// AddTestBucket adds a fake bucket for testing.
+func (c *GCSClient) AddTestBucket(name string, bh BucketHandle) {
+	if c.buckets == nil {
+		c.buckets = make(map[string]BucketHandle, 5)
+	}
+	c.buckets[name] = bh
+}
+
+// Close implements stiface.Client.Close
+func (c GCSClient) Close() error {
+	return nil
+}
+
+// Bucket implements stiface.Client.Bucket
+func (c GCSClient) Bucket(name string) stiface.BucketHandle {
+	return c.buckets[name]
+}
+
+// BucketHandle provides a fake BucketHandle implementation for testing.
+type BucketHandle struct {
+	stiface.BucketHandle
+	ObjAttrs []*storage.ObjectAttrs // Objects that will be returned by iterator
+}
+
+// Attrs implements trivial stiface.BucketHandle.Attrs
+func (bh BucketHandle) Attrs(ctx context.Context) (*storage.BucketAttrs, error) {
+	return &storage.BucketAttrs{}, nil
+}
+
+// Objects implements stiface.BucketHandle.Objects
+func (bh BucketHandle) Objects(ctx context.Context, q *storage.Query) stiface.ObjectIterator {
+	// TODO - should check if ctx has expired?
+	obj := make([]*storage.ObjectAttrs, 0, len(bh.ObjAttrs))
+	dir := ""
+	for i := range bh.ObjAttrs {
+		if !strings.HasPrefix(bh.ObjAttrs[i].Name, q.Prefix) {
+			continue
+		}
+		if q.Delimiter != "" {
+			suffix := strings.Trim(bh.ObjAttrs[i].Name[len(q.Prefix):], q.Delimiter)
+			parts := strings.Split(suffix, q.Delimiter)
+			if len(parts) > 1 {
+				if dir != parts[0] {
+					dir = parts[0]
+					obj = append(obj, &storage.ObjectAttrs{Prefix: strings.Trim(q.Prefix, q.Delimiter) + q.Delimiter + parts[0]})
+				}
+				continue
+			}
+		}
+		obj = append(obj, bh.ObjAttrs[i])
+	}
+	n := 0
+	return objIt{next: &n, objects: obj}
+}
+
+// objIt provides a fake stiface.ObjectIterator
+type objIt struct {
+	stiface.ObjectIterator
+	objects []*storage.ObjectAttrs
+	next    *int
+}
+
+// Next implements stiface.ObjectIterator.Next
+func (it objIt) Next() (*storage.ObjectAttrs, error) {
+	if *it.next >= len(it.objects) {
+		return nil, iterator.Done
+	}
+	*it.next++
+	return it.objects[*it.next-1], nil
+}
---
+++ cloudtest/gcs_test.go
@@ -0,0 +1,99 @@
+package cloudtest_test
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/GoogleCloudPlatform/google-cloud-go-testing/storage/stiface"
+	"github.com/m-lab/go/cloudtest"
+	"google.golang.org/api/iterator"
+)
+
+func init() {
+	// Always prepend the filename and line number.
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func assertStifaceClient() { func(c stiface.Client) {}(&cloudtest.GCSClient{}) }
+
+func countAll(t *testing.T, it stiface.ObjectIterator) (total int, normal int, prefix int) {
+	for o, err := it.Next(); err != iterator.Done; o, err = it.Next() {
+		if err != nil {
+			t.Fatal(err, "when attempting it.Next()")
+			continue
+		}
+
+		total++
+
+		if o.Prefix != "" {
+			prefix++
+			log.Println("Skipping", o.Prefix)
+			continue
+		}
+		if o.Updated.Before(time.Now().Add(-time.Minute)) {
+			continue
+		}
+		normal++
+	}
+	return
+}
+
+func TestGCSClient(t *testing.T) {
+	// Use a fake queue client.
+	ctx := context.Background()
+
+	fc := cloudtest.GCSClient{}
+	fc.AddTestBucket("foobar",
+		cloudtest.BucketHandle{
+			ObjAttrs: []*storage.ObjectAttrs{
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/obj1", Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/obj2", Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/obj3"},                             // Will be filtered out by the "since" filter.
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/subdir/obj4", Updated: time.Now()}, // filtered because of subdir.
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/subdir/obj5", Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "obj6", Updated: time.Now()}, // Will be filtered by prefix.
+			}})
+
+	bucket := fc.Bucket("foobar")
+	if bucket == nil {
+		t.Fatal("Bucket is nil")
+	}
+	// Check that the bucket is valid, by fetching it's attributes.
+	// Bypass check if we are running travis tests.
+	_, err := bucket.Attrs(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+
+	type test struct {
+		prefix string
+		n      int
+		p      int
+	}
+	tests := []test{
+		{"ndt/2019/01/01", 2, 1},     // Prefix that is a directory, but without the final /
+		{"ndt/2019/01/01/", 2, 1},    // Should work both with and without slash.
+		{"ndt/2019/01/01/obj", 2, 0}, // Should work with a prefix that isn't a directory.
+	}
+
+	for _, tt := range tests {
+		qry := storage.Query{
+			Delimiter: "/",
+			Prefix:    tt.prefix,
+		}
+		it := bucket.Objects(ctx, &qry)
+
+		_, n, p := countAll(t, it)
+		if n != tt.n {
+			t.Error("Expected", tt.n, "items, got", n)
+		}
+		if p != tt.p {
+			t.Error("Expected", tt.p, "prefix, got", p)
+		}
+	}
+
+	fc.Close()
+}
