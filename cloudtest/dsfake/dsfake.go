51f958a147521ab2952785939b2faa9c3f0e5551
Gregory Russell <gfr10598@users.noreply.github.com>
2019-12-02 19:41:36 -0500

    Add dsfake.Client minimal fake datastore client (#78)
    * Add dsfake.Client minimal fake datastore client
    
    * add test the exposes bug
    
    * test and fix local mod bug
    
    * cleanup spam

---
+++ cloudtest/dsfake/dsfake.go
@@ -0,0 +1,115 @@
+// Package dsfake implements a fake dsiface.Client
+// If you make changes to existing code, please test whether it breaks
+// existing clients, e.g. in etl-gardener.
+package dsfake
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"log"
+	"reflect"
+	"sync"
+
+	"cloud.google.com/go/datastore"
+	"github.com/GoogleCloudPlatform/google-cloud-go-testing/datastore/dsiface"
+)
+
+// NOTE: This is over-restrictive, but fine for current purposes.
+func validateDatastoreEntity(e interface{}) error {
+	v := reflect.ValueOf(e)
+	if v.Kind() != reflect.Ptr {
+		return datastore.ErrInvalidEntityType
+	}
+	// NOTE: This is over-restrictive, but fine for current purposes.
+	if reflect.Indirect(v).Kind() != reflect.Struct {
+		return datastore.ErrInvalidEntityType
+	}
+	return nil
+}
+
+// ErrNotImplemented is returned if a dsiface function is unimplemented.
+var ErrNotImplemented = errors.New("Not implemented")
+
+// Client implements a crude datastore test client.  It is somewhat
+// simplistic and incomplete.  It works only for basic Put, Get, and Delete,
+// but may not always work correctly.
+type Client struct {
+	dsiface.Client // For unimplemented methods
+	lock           sync.Mutex
+	objects        map[datastore.Key][]byte
+}
+
+// NewClient returns a fake client that satisfies dsiface.Client.
+func NewClient() *Client {
+	if flag.Lookup("test.v") == nil {
+		log.Fatal("DSFakeClient should only be used in tests")
+	}
+	return &Client{objects: make(map[datastore.Key][]byte, 10)}
+}
+
+// Close implements dsiface.Client.Close
+func (c *Client) Close() error { return nil }
+
+// Count implements dsiface.Client.Count
+func (c *Client) Count(ctx context.Context, q *datastore.Query) (n int, err error) {
+	return 0, ErrNotImplemented
+}
+
+// Delete implements dsiface.Client.Delete
+func (c *Client) Delete(ctx context.Context, key *datastore.Key) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	_, ok := c.objects[*key]
+	if !ok {
+		return datastore.ErrNoSuchEntity
+	}
+	delete(c.objects, *key)
+	return nil
+}
+
+// Get implements dsiface.Client.Get
+func (c *Client) Get(ctx context.Context, key *datastore.Key, dst interface{}) (err error) {
+	err = validateDatastoreEntity(dst)
+	if err != nil {
+		return err
+	}
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	o, ok := c.objects[*key]
+	if !ok {
+		return datastore.ErrNoSuchEntity
+	}
+	return json.Unmarshal(o, dst)
+}
+
+// Put mplements dsiface.Client.Put
+func (c *Client) Put(ctx context.Context, key *datastore.Key, src interface{}) (*datastore.Key, error) {
+	err := validateDatastoreEntity(src)
+	if err != nil {
+		return nil, err
+	}
+	js, err := json.Marshal(src)
+	if err != nil {
+		return nil, err
+	}
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.objects[*key] = js
+	return key, nil
+}
+
+// GetKeys lists all keys saved in the fake client.
+func (c *Client) GetKeys() []datastore.Key {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	keys := make([]datastore.Key, len(c.objects))
+	i := 0
+	for k := range c.objects {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
---
+++ cloudtest/dsfake/dsfake_test.go
@@ -0,0 +1,89 @@
+package dsfake_test
+
+import (
+	"log"
+	"testing"
+
+	"cloud.google.com/go/datastore"
+	"github.com/m-lab/go/cloudtest/dsfake"
+)
+
+func init() {
+	// Always prepend the filename and line number.
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func must(t *testing.T, err error) {
+	if err != nil {
+		log.Output(2, err.Error())
+		t.Fatal(err)
+	}
+}
+
+type Object struct {
+	Value string
+}
+
+func TestDSFake(t *testing.T) {
+	client := dsfake.NewClient()
+
+	k1 := datastore.NameKey("TestDSFake", "o1", nil)
+	k1.Namespace = "dsfake"
+	k2 := datastore.NameKey("TestDSFake", "o2", nil)
+	k2.Namespace = "dsfake"
+
+	o1 := Object{"o1"}
+	_, err := client.Put(nil, k1, &o1)
+	must(t, err)
+
+	var o1a Object
+	// This should fail because it o1a isn't a pointer
+	err = client.Get(nil, k1, o1a)
+	if err != datastore.ErrInvalidEntityType {
+		t.Error("Should detect non-pointer")
+	}
+
+	must(t, client.Get(nil, k1, &o1a))
+	if o1a.Value != o1.Value {
+		t.Fatal("Failed put/get", o1a, o1)
+	}
+
+	// A second object should not interfere with the first.
+	o2 := Object{"o2"}
+	_, err = client.Put(nil, k2, &o2)
+	must(t, err)
+
+	// Check that Get still fetches the correct o1 value
+	var o1b Object
+	must(t, client.Get(nil, k1, &o1b))
+	if o1b.Value != o1.Value {
+		t.Fatal("Apparent object collision", o1b, o1)
+	}
+
+	o2.Value = "local-o2"
+	// Check that changing original object doesn't change the stored value.
+	var o2a Object
+	must(t, client.Get(nil, k2, &o2a))
+	if o2a.Value != "o2" {
+		t.Error("Changing local modifies persisted value", o2a.Value, "!=", "o2")
+	}
+
+	keys := client.GetKeys()
+	if len(keys) != 2 {
+		t.Fatal("Should be 2 keys")
+	}
+	if keys[0] != *k1 && keys[1] != *k1 {
+		t.Error("Missing key", k1)
+	}
+	if keys[0] != *k2 && keys[1] != *k2 {
+		t.Error("Missing key", k2)
+	}
+
+	// Test Delete()
+	must(t, client.Delete(nil, k1))
+	err = client.Get(nil, k1, &o1b)
+	if err != datastore.ErrNoSuchEntity {
+		t.Fatal("delete failed")
+	}
+
+}
