43610d3387ffc914b3b1625e204d234397ad6145
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-12-04 22:00:17 -0500

    Complete coverage

--- logx/debug_test.go
+++ logx/debug_test.go
@@ -6,23 +6,25 @@ import (
 	"testing"
 )
 
-func TestSetup(t *testing.T) {
+func TestLogxDebug(t *testing.T) {
 	tests := []struct {
 		name     string
 		enable   string
 		msg      string
 		expected string
-		wantErr  bool
+		want     bool
 	}{
 		{
 			name:     "success-setup-enable",
 			msg:      "this is a test message",
 			expected: "DEBUG: this is a test message\n",
 			enable:   "true",
+			want:     true,
 		},
 		{
 			name:   "success-setup-disabled",
 			enable: "false",
+			want:   false,
 		},
 	}
 	for _, tt := range tests {
@@ -32,6 +34,9 @@ func TestSetup(t *testing.T) {
 			log.SetOutput(buf)
 
 			LogxDebug.Set(tt.enable)
+			if LogxDebug.Get() != tt.want {
+				t.Errorf("LogxDebug.Get return wrong value; got %t, want %t", LogxDebug.Get(), tt.want)
+			}
 
 			Debug.Print(tt.msg)
 			got := string(buf.Bytes())
