b0b1f6452d7cef5064a3ef25d46a923e55979f0b
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-02-19 23:57:17 -0500

    Add some clarifying comments to tests

--- flagx/url_test.go
+++ flagx/url_test.go
@@ -42,19 +42,24 @@ func TestURL(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// Attempt to set the URL.
 			u := flagx.URL{}
 			if err := u.Set(tt.s); (err != nil) != tt.wantErr {
 				t.Errorf("URL.Set() error = %v, wantErr %v", err, tt.wantErr)
 			}
+			// Skip remaining checks if we expect an error.
 			if tt.wantErr {
 				return
 			}
+			// Does the original URL match the reformatted URL?
 			if u.String() != tt.s {
 				t.Errorf("URL.String() got = %q, want %q", u.String(), tt.s)
 			}
+			// Does the parsed URL match the expected URL?
 			if diff := deep.Equal(u.Get(), tt.want); diff != nil {
 				t.Errorf("URL.Set()\ngot %#v\nwant %#v\ndifferences: %v", u.URL, tt.want, diff)
 			}
+			// Try to initialize a flag using the original test value.
 			s := flagx.MustNewURL(tt.s)
 			if diff := deep.Equal(u, s); diff != nil {
 				t.Errorf("URL.Set and MustNewURL returned different values: %#v", diff)
