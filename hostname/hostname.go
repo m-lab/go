80a9a991dd4675f542844cff11fb960c51adbc08
Nathan Kinkade <kinkade@measurementlab.net>
2020-03-20 11:40:38 -0600

    Adds a new package hostname for parsing v1 and v2 platform hostnames.

---
+++ hostname/hostname.go
@@ -0,0 +1,64 @@
+// Package hostname parses v1 and v2 hostnames into their constituent parts. It
+// is intended to help in the transition from v1 to v2 names on the platform.
+// Other Go programs should not have to worry about how to parse hostnames.
+package hostname
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// Hostname represents an M-Lab hostname and all of its constituent parts.
+type Hostname struct {
+	hostname string
+	machine  string
+	site     string
+	project  string
+	domain   string
+	version  string
+}
+
+// Parse parses an M-Lab hostname and breaks it into its constituent parts.
+func Parse(name string) (Hostname, error) {
+	var parts Hostname
+
+	reInit := regexp.MustCompile(`^mlab[1-4]([.-])`)
+	reV1 := regexp.MustCompile(`^(mlab[1-4])\.([a-z]{3}[0-9tc]{2})\.(.*)$`)
+	reV2 := regexp.MustCompile(`^(mlab[1-4])-([a-z]{3}[0-9tc]{2})\.(.*?)\.(.*)$`)
+
+	mInit := reInit.FindAllStringSubmatch(name, -1)
+	if len(mInit) != 1 || len(mInit[0]) != 2 {
+		return parts, fmt.Errorf("Invalid hostname: %s", name)
+	}
+
+	switch mInit[0][1] {
+	case "-":
+		mV2 := reV2.FindAllStringSubmatch(name, -1)
+		if len(mV2) != 1 || len(mV2[0]) != 5 {
+			return parts, fmt.Errorf("Invalid v2 hostname: %s", name)
+		}
+		parts = Hostname{
+			hostname: mV2[0][0],
+			machine:  mV2[0][1],
+			site:     mV2[0][2],
+			project:  mV2[0][3],
+			domain:   mV2[0][4],
+			version:  "v2",
+		}
+	case ".":
+		mV1 := reV1.FindAllStringSubmatch(name, -1)
+		if len(mV1) != 1 || len(mV1[0]) != 4 {
+			return parts, fmt.Errorf("Invalid v1 hostname: %s", name)
+		}
+		parts = Hostname{
+			hostname: mV1[0][0],
+			machine:  mV1[0][1],
+			site:     mV1[0][2],
+			project:  "",
+			domain:   mV1[0][3],
+			version:  "v1",
+		}
+	}
+
+	return parts, nil
+}
---
+++ hostname/hostname_test.go
@@ -0,0 +1,77 @@
+package hostname
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHostname(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    Hostname
+		wantErr bool
+	}{
+		{
+			name: "valid-v1",
+			want: Hostname{
+				hostname: "mlab1.lol01.measurement-lab.org",
+				machine:  "mlab1",
+				site:     "lol01",
+				project:  "",
+				domain:   "measurement-lab.org",
+				version:  "v1",
+			},
+		},
+		{
+			name: "valid-v2",
+			want: Hostname{
+				hostname: "mlab1-lol01.mlab-oti.measurement-lab.org",
+				machine:  "mlab1",
+				site:     "lol01",
+				project:  "mlab-oti",
+				domain:   "measurement-lab.org",
+				version:  "v2",
+			},
+		},
+		{
+			name: "invalid-v1-bad-separator",
+			want: Hostname{
+				hostname: "mlab1=lol01.measurement-lab.org",
+				machine:  "mlab1",
+				site:     "lol01",
+				project:  "",
+				domain:   "measurement-lab.org",
+				version:  "v1",
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid-v2-dotted-host",
+			want: Hostname{
+				hostname: "mlab1.lol01.mlab-staging.measurement-lab.org",
+				machine:  "mlab1",
+				site:     "lol01",
+				project:  "mlab-staging",
+				domain:   "measurement-lab.org",
+				version:  "v2",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := Parse(test.want.hostname)
+		// If we wanted an err but didn't get one, or vice versa, then fail.
+		if (err != nil) != test.wantErr {
+			t.Errorf("hostname.Parse() error %v, wantErr %v", err, test.wantErr)
+		}
+		// If we wanted an err, stop this particulat test, since later tests will fail.
+		if test.wantErr == true {
+			t.Skipf("wantErr == true: not continuing test: %s", test.name)
+		}
+		if !reflect.DeepEqual(result, test.want) {
+			t.Errorf("\nUnexpected result. Got:\n%+v\nExpected:\n%+v", result, test.want)
+		}
+
+	}
+}
