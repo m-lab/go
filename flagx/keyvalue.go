3a45fdbfb243efedb536a28f726669305d1b318f
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-02-14 16:00:56 -0500

    Extend KeyValue to read value from file

--- flagx/keyvalue.go
+++ flagx/keyvalue.go
@@ -2,47 +2,73 @@ package flagx
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
-// KeyValue is a way of setting the elements of a map[string]string individually
-// as key-value pairs on the command-line. It is designed to be used as a
-// repeated argument, where each invocation of the command-line argument will
-// add another key value pair.
-//
-// One use for this library could be to specify metadata headers.
+// KeyValue parses "key=value" pairs from a given argument. The KeyValue flag is
+// designed to be used for repeatable arguments. Each use of the flag will add
+// another key value pair (or overwrite a previous one if the same key is used).
 type KeyValue struct {
-	pairs map[string]string
+	pairs map[string]kvsource
 }
 
-// Set breaks the string apart at the first equals sign and puts the result
-// key-value pair into the map.
-func (kv *KeyValue) Set(kvstring string) error {
-	pair := strings.SplitN(kvstring, "=", 2)
-	if len(pair) != 2 {
-		return fmt.Errorf("Bad kay value pair %q split on '=' into %d pieces (should have been 2)", kvstring, len(pair))
+type kvsource struct {
+	value string
+	fname string
+}
+
+// Set parses key=value argument. Multiple pairs may be separated with a comma,
+// i.e. "key1=value1,key2=value2". When the first character of the value is
+// prefixed by with "@", i.e. "key1=@file1", Set reads the file content for the
+// key value.
+func (kv *KeyValue) Set(kvs string) error {
+	pairs := strings.Split(kvs, ",")
+	for _, pair := range pairs {
+		fields := strings.SplitN(pair, "=", 2)
+		if len(fields) != 2 {
+			return fmt.Errorf("bad key/value: %q split on '=' into %d pieces (should have been 2)", pair, len(fields))
+		}
+		if kv.pairs == nil {
+			kv.pairs = make(map[string]kvsource)
+		}
+		if len(fields[1]) > 0 && fields[1][0] == '@' {
+			fname := fields[1][1:]
+			b, err := ioutil.ReadFile(fname)
+			if err != nil {
+				return err
+			}
+			kv.pairs[fields[0]] = kvsource{
+				value: string(b),
+				fname: fname,
+			}
+		} else {
+			kv.pairs[fields[0]] = kvsource{value: fields[1]}
+		}
 	}
-	if kv.pairs == nil {
-		kv.pairs = make(map[string]string)
-	}
-	kv.pairs[pair[0]] = pair[1]
 	return nil
 }
 
-// String converts the headers into a text representation. It's not expected to
-// provide useful output here, but it is a function that is required in order to
-// implement flag.Value.
+// String serializes parsed arguments into a form similiar to how they were
+// added from the command line. Order is not preserved.
 func (kv *KeyValue) String() string {
-	return fmt.Sprintf("%+v", kv.pairs)
+	fields := []string{}
+	for k, s := range kv.pairs {
+		if s.fname == "" {
+			fields = append(fields, fmt.Sprintf("%s=%s", k, s.value))
+		} else {
+			fields = append(fields, fmt.Sprintf("%s=@%s", k, s.fname))
+		}
+	}
+	return strings.Join(fields, ",")
 }
 
-// Get returns a copy of the KeyValue object as a map[string]string. This new
-// map may be modified as desired without worrying about modifying the
-// command-line arguments.
+// Get returns all of the KeyValue pairs as a map[string]string. The returned
+// value is a copy.
 func (kv *KeyValue) Get() map[string]string {
 	h := make(map[string]string)
-	for k, v := range kv.pairs {
-		h[k] = v
+	for k, s := range kv.pairs {
+		h[k] = s.value
 	}
 	return h
 }
--- flagx/keyvalue_test.go
+++ flagx/keyvalue_test.go
@@ -2,31 +2,134 @@ package flagx_test
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
-	"reflect"
+	"os"
+	"path"
+	"sort"
+	"strings"
 	"testing"
 
+	"github.com/go-test/deep"
+
 	"github.com/m-lab/go/flagx"
-	"github.com/m-lab/go/rtx"
 )
 
-func TestKeyValue(t *testing.T) {
-	kv := &flagx.KeyValue{}
-	rtx.Must(kv.Set("MLAB.thing1=thing2"), "Could not set thing1 to thing2")
-	rtx.Must(kv.Set("MLAB.a=b"), "Could not set a to b")
-	err := kv.Set("MLAB.c:d")
-	if err == nil {
-		t.Error("Should have had an error inserting the keypair MLAB.c:d")
+func mapsMatch(src, dst map[string]string) bool {
+	for key, val := range src {
+		if dst[key] != val {
+			return false
+		}
 	}
-	if !reflect.DeepEqual(kv.Get(), map[string]string{
-		"MLAB.thing1": "thing2",
-		"MLAB.a":      "b",
-	}) {
-		t.Errorf("%v is wrong", kv.String())
+	for key, val := range dst {
+		if src[key] != val {
+			return false
+		}
 	}
+	return true
+}
 
-	if kv.String() == "" {
-		t.Error("KeyValue.String() isn't expected to be useful, but it should not be empty")
+func TestKeyValue(t *testing.T) {
+	d := t.TempDir()
+	f := path.Join(d, "args.txt")
+	tests := []struct {
+		name    string
+		kvs     string
+		file    string
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "success-single-keyvalue",
+			kvs:  "a=b",
+			want: map[string]string{
+				"a": "b",
+			},
+		},
+		{
+			name: "success-multiple-keyvalue",
+			kvs:  "a=b,c=d",
+			want: map[string]string{
+				"a": "b",
+				"c": "d",
+			},
+		},
+		{
+			name: "success-single-from-file",
+			kvs:  "c=@" + f,
+			file: "d",
+			want: map[string]string{
+				"c": "d",
+			},
+		},
+		{
+			name: "success-multiple-from-file",
+			kvs:  "a=b,c=@" + f,
+			file: "d",
+			want: map[string]string{
+				"a": "b",
+				"c": "d",
+			},
+		},
+		{
+			name: "success-multiple-from-file-2",
+			kvs:  "a=@" + f + ",c=@" + f,
+			file: "d",
+			want: map[string]string{
+				"a": "d",
+				"c": "d",
+			},
+		},
+		{
+			name:    "error-bad-key-value",
+			kvs:     "a=b,c",
+			wantErr: true,
+		},
+		{
+			name:    "error-missing-file",
+			kvs:     "a=b,c=@",
+			wantErr: true,
+		},
+		{
+			name:    "error-not-a-file",
+			kvs:     "a=b,c=@/",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.file != "" {
+				// write an args file to a tempdir.
+				err := ioutil.WriteFile(f, []byte(tt.file), os.ModePerm)
+				if err != nil {
+					t.Fatalf("FlagsFromFile write file failed: %v", err)
+				}
+			}
+
+			// Create kv flag.
+			kv := &flagx.KeyValue{}
+			if err := kv.Set(tt.kvs); (err != nil) != tt.wantErr {
+				t.Errorf("KeyValue.Set() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr {
+				return
+			}
+
+			// Get returns a map with expected values.
+			got := kv.Get()
+			if !mapsMatch(got, tt.want) {
+				t.Errorf("KeyValue.Get() did not match; got = %v, want %v", got, tt.want)
+			}
+
+			// String returns the same fields given. Parse b/c order is not guaranteed.
+			strFields := strings.Split(kv.String(), ",")
+			sort.Strings(strFields)
+			kvsFields := strings.Split(tt.kvs, ",")
+			sort.Strings(kvsFields)
+			if diff := deep.Equal(strFields, kvsFields); diff != nil {
+				t.Errorf("KeyValue.String() did not match; got = %v, want %v, diff %v", strFields, kvsFields, diff)
+			}
+		})
 	}
 }
 
