100e86b04fa748f7967b94abda3f6ae7c5596d8a
Cristina Leon <cristinaleon@google.com>
2023-11-28 02:30:15 +0000

    Docstrings

--- mathx/rand.go
+++ mathx/rand.go
@@ -7,6 +7,8 @@ import (
 
 // GetRandomInt returns a non-negative pseudo-random number in the interval [0, max).
 // It returns 0 if max <= 0.
+// NOTE: this function uses the default Source from the math/rand package. This source
+// is only seeded with a random value since go 1.20.
 func GetRandomInt(max int) int {
 	if max <= 0 {
 		return 0
@@ -17,6 +19,8 @@ func GetRandomInt(max int) int {
 // GetExpDistributedInt returns a exponentially distributed number in the interval
 // [0, +math.MaxFloat64), rounded to the nearest int. Callers can adjust the rate of the
 // function through the rate parameter.
+// NOTE: this function uses the default Source from the math/rand package. This source
+// is only seeded with a random value since go 1.20.
 func GetExpDistributedInt(rate float64) int {
 	f := rand.ExpFloat64() / rate
 	index := int(math.Round(f))
