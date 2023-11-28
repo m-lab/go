f6c42a521912e8032f236ceb2459969de10ea8a2
Cristina Leon <cristinaleon@google.com>
2023-11-28 01:52:43 +0000

    Use the default source for randomization

--- mathx/rand.go
+++ mathx/rand.go
@@ -5,32 +5,20 @@ import (
 	"math/rand"
 )
 
-// Random is a source of random values.
-type Random struct {
-	Src *rand.Rand // Pseudo-random source seeded with a given value.
-}
-
-// NewRandom returns a new Random that uses the provided seed to generate
-// random values.
-func NewRandom(seed int64) Random {
-	src := rand.New(rand.NewSource(seed))
-	return Random{Src: src}
-}
-
 // GetRandomInt returns a non-negative pseudo-random number in the interval [0, max).
 // It returns 0 if max <= 0.
-func (r *Random) GetRandomInt(max int) int {
+func GetRandomInt(max int) int {
 	if max <= 0 {
 		return 0
 	}
-	return r.Src.Intn(max)
+	return rand.Intn(max)
 }
 
 // GetExpDistributedInt returns a exponentially distributed number in the interval
 // [0, +math.MaxFloat64), rounded to the nearest int. Callers can adjust the rate of the
 // function through the rate parameter.
-func (r *Random) GetExpDistributedInt(rate float64) int {
-	f := r.Src.ExpFloat64() / rate
+func GetExpDistributedInt(rate float64) int {
+	f := rand.ExpFloat64() / rate
 	index := int(math.Round(f))
 	return index
 }
